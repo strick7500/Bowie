47393b9a77030c899a61835872defe8524f0dd19
Adam <strick7500@gmail.com>
2019-10-23 14:05:14 -0500

    Update game
    Add imports, define play()

--- game.py
+++ game.py
@@ -6,3 +6,11 @@ Created on Wed Oct 23 14:03:32 2019
 @author: adamstrick
 """
 
+import world
+from player import Player
+
+def play():
+    world.load_tiles()
+    player = Player()
+    while player.is_alive() and not player.victory:
+        #Loop begins here
